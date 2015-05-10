81163af6a9303faab2924caf0cbb9bcd667dd2e8
MoonBlade <moonblade168@gmail.com>
2015-05-11 00:19:35 +0530

    removes sales tab

--- dist/js/pages/dashboard.js
+++ dist/js/pages/dashboard.js
@@ -13,7 +13,7 @@ function exportpdf(){
   $('#pressure-table').tableExport({type:'pdf',escape:'false'});
 }
 
-$(function () {
+// $(function () {
 
   //Make the dashboard widgets sortable Using jquery UI
   $(".connectedSortable").sortable({
@@ -132,27 +132,7 @@ $(function () {
 
   /* Morris.js Charts */
   // Sales chart
-  var area = new Morris.Area({
-    element: 'revenue-chart',
-    resize: true,
-    data: [
-      {y: '2011 Q1', item1: 2666, item2: 2666},
-      {y: '2011 Q2', item1: 2778, item2: 2294},
-      {y: '2011 Q3', item1: 4912, item2: 1969},
-      {y: '2011 Q4', item1: 3767, item2: 3597},
-      {y: '2012 Q1', item1: 6810, item2: 1914},
-      {y: '2012 Q2', item1: 5670, item2: 4293},
-      {y: '2012 Q3', item1: 4820, item2: 3795},
-      {y: '2012 Q4', item1: 15073, item2: 5967},
-      {y: '2013 Q1', item1: 10687, item2: 4460},
-      {y: '2013 Q2', item1: 8432, item2: 5713}
-    ],
-    xkey: 'y',
-    ykeys: ['item1', 'item2'],
-    labels: ['Item 1', 'Item 2'],
-    lineColors: ['#a0d0e0', '#3c8dbc'],
-    hideHover: 'auto'
-  });
+
   var line = new Morris.Line({
     element: 'line-chart',
     resize: true,
@@ -183,27 +163,13 @@ $(function () {
     gridTextSize: 10
   });
 
-  //Donut Chart
-  var donut = new Morris.Donut({
-    element: 'sales-chart',
-    resize: true,
-    colors: ["#3c8dbc", "#f56954", "#00a65a"],
-    data: [
-      {label: "Download Sales", value: 12},
-      {label: "In-Store Sales", value: 30},
-      {label: "Mail-Order Sales", value: 20}
-    ],
-    hideHover: 'auto'
-  });
-
 var saledonot = new Morris.Donut({
     element: 'saledonot',
     resize: true,
-    colors: ["#3c8dbc", "#f56954", "#00a65a"],
+    colors: ["#3c8dbc", "#fff", "#00a65a"],
     data: [
       {label: "Floor 1 Pressure", value: 2},
-      {label: "Floor 2 Pressure", value: 1},
-      {label: "Floor 3 Pressure", value: 1}
+      {label: "Floor 1 Pressure", value: 1}
     ],
     hideHover: 'auto'
   });
@@ -278,4 +244,4 @@ $(document).ready(apicall);
     }
   });
 
-});
+// });
